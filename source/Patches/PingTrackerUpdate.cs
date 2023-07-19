48dc78e7d56e53aafbab614d20eafddd6b905c5f
Andrea Marucci <itscrystal2k@gmail.com>
2023-07-19 20:30:12 +0200

    Banner Update + Credits Fix

--- source/Patches/PingTrackerUpdate.cs
+++ source/Patches/PingTrackerUpdate.cs
@@ -19,11 +19,10 @@ namespace TownOfUs
                 "<color=#00FF00FF>TownOfUs v" + TownOfUs.VersionString + "</color> <color=#ff0000ff>I</color><color=#ffffffff>T</color><color=#00ff00ff>A</color>\n" +
                 $"Ping: {AmongUsClient.Instance.Ping}ms\n" +
                 (!MeetingHud.Instance
-                    ? "<color=#00FF00FF>Modded By: Donners &</color>\n" +
-                    "<color=#00FF00FF>MyDragonBreath</color>\n" : "") +
+                    ? "<color=#FF0000FF>Modded By:</color> <color=#FF0000FF>Donners & MyDragonBreath</color>\n" : "") +
                 (AmongUsClient.Instance.GameState != InnerNet.InnerNetClient.GameStates.Started
-                    ? "<color=#00FF00FF>Formerly: Slushiegoose & Polus.gg</color>\n"+
-                    "<color=#00FF00FF>Italian Version:</color><color=#FF0000FF>Crystal2K & Rogertarta</color>" : "");
+                    ? "<color=#FF0000FF>Formerly:</color> <color=#FF0000FF>Slushiegoose & Polus.gg</color>\n"+
+                    "<color=#FF0000FF>Italian Version: </color><color=#00FF00FF>Crystal2K & Rogertarta</color>" : "");
         }
     }
 }

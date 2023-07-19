9c3bedbc09ce3a1291b3d0661243874b5da0891e
Andrea Marucci <itscrystal2k@gmail.com>
2023-07-19 19:10:36 +0200

    ITA v1.0.0
    Added italian version credits on in-game HUD and hat "Undertale Sunflower"

--- source/Patches/PingTrackerUpdate.cs
+++ source/Patches/PingTrackerUpdate.cs
@@ -16,13 +16,14 @@ namespace TownOfUs
             position.AdjustPosition();
 
             __instance.text.text =
-                "<color=#00FF00FF>TownOfUs v" + TownOfUs.VersionString + "</color>\n" +
+                "<color=#00FF00FF>TownOfUs v" + TownOfUs.VersionString + "</color> <color=#ff0000ff>I</color><color=#ffffffff>T</color><color=#00ff00ff>A</color>\n" +
                 $"Ping: {AmongUsClient.Instance.Ping}ms\n" +
                 (!MeetingHud.Instance
                     ? "<color=#00FF00FF>Modded By: Donners &</color>\n" +
                     "<color=#00FF00FF>MyDragonBreath</color>\n" : "") +
                 (AmongUsClient.Instance.GameState != InnerNet.InnerNetClient.GameStates.Started
-                    ? "<color=#00FF00FF>Formerly: Slushiegoose & Polus.gg</color>" : "");
+                    ? "<color=#00FF00FF>Formerly: Slushiegoose & Polus.gg</color>\n"+
+                    "<color=#00FF00FF>Italian Version:</color><color=#FF0000FF>Crystal2K & Rogertarta</color>" : "");
         }
     }
 }
